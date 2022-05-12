aa05af39fd8ea66bbbf65b5a52bb438caeb493fe
@reroes <reroes799@gmail.com>
2022-05-12 09:46:01 -0500

    ejemplo de aplicación con Android

---
+++ MiAplicacion001/app/src/main/java/com/example/miaplicacion001/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.miaplicacion001;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, MainActivity2.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
